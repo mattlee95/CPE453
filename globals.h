d311602caf47b84c15c9904a18d66a060e4b716d
Matt Lee <mattlee@Matts-MacBook-Pro.local>
2018-06-09 18:18:29 -0700

    global typing error

--- globals.h
+++ globals.h
@@ -3,13 +3,12 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-#include <stdint.h>
 //place defines and prototypes here
 char song_name[100];
-uint32_t curr_song_idx;
-uint32_t num_songs;
-uint32_t song_inodes[2000]; 
-uint32_t song_dur;
-uint32_t curr_dur;
+unsigned int curr_song_idx;
+unsigned int num_songs;
+unsigned int song_inodes[2000]; 
+unsigned int song_dur;
+unsigned int curr_dur;
 
 #endif
